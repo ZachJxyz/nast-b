704ed862bb66111584c9aac03387061fecb1f75f
Zach <73715397+ZachVJ@users.noreply.github.com>
2024-03-17 20:58:26 -0400

    typeform for contact page

--- ajax.php
+++ ajax.php
@@ -1,22 +1,22 @@
 <?php
 if(isset($_POST) && !empty($_POST)){
-	$full_name = (isset($_POST['full_name']))?$_POST['full_name']:'';
+	// $full_name = (isset($_POST['full_name']))?$_POST['full_name']:'';
 	$first_name = (isset($_POST['first_name']))?$_POST['first_name']:'';
-	$middle_name = (isset($_POST['middle_name']))?$_POST['middle_name']:'';
-	$last_name = (isset($_POST['last_name']))?$_POST['last_name']:'';
+	// $middle_name = (isset($_POST['middle_name']))?$_POST['middle_name']:'';
+	// $last_name = (isset($_POST['last_name']))?$_POST['last_name']:'';
 	$email = (isset($_POST['email']))?$_POST['email']:'';
 	$subject = (isset($_POST['subject']))?$_POST['subject']:'';
 	$message = (isset($_POST['message']))?$_POST['message']:'';
 	$contact_no = (isset($_POST['contact_no']))?$_POST['contact_no']:'';
 	
-	if($full_name == ''){
-		$full_name =  $first_name.' '.$middle_name.' '.$last_name;
-	}
+	// if($full_name == ''){
+	// 	$full_name =  $first_name.' '.$middle_name.' '.$last_name;
+	// }
 	
 	$sendMessage = $mailSubject = '';
 	if($_POST['form_type'] == 'contact'){
 		$mailSubject = 'Contact Details';
-		$sendMessage = "<p>Hello,</p><p>".$full_name." has sent a message having </p><p><b>Subject:</b> ".$subject."</p><p><b>Email id:</b> ".$email."</p><p><b>Query is:</b> ".$message."</p>";
+		$sendMessage = "<p>Hello,</p><p>".$first_name." has sent a message having </p><p><b>Subject:</b> ".$subject."</p><p><b>Email id:</b> ".$email."</p><p><b>Query is:</b> ".$message."</p>";
 	}elseif($_POST['form_type'] == 'inquiry'){
 		$mailSubject = 'Inquiry Details';
 		$sendMessage = '';
